3a265c809292152fc69e5c8f07f8b4e44b1779a6
praveen kumar <programmerpraveenkumar@gmail.com>
2024-10-18 21:28:23 +0530

    files added

--- reactjs/app/src/Home.js
+++ reactjs/app/src/Home.js
@@ -2,11 +2,30 @@ import { Header } from "./Header";
 
 export function Home(){
     let status  = true;
+    let num1 = 34;
+    let num2 = 75;
+    const showMsg=()=>{
+        let res = num1+num2
+        console.log(res);
+    }
     return (
         <>
             <Header/>
             <h1>Home component</h1>
+
             {(status)?<h2>sample title</h2>:null}
+
+            <h2 className ={(status)?"red":null}>this is h2</h2>
+            {/* 
+                create new element calld h3
+                create css class 'blue'
+                create a variable called bgBlue.
+                if bgBlue value is true show the blue color or show as it is.
+                
+
+            */}
+            <button onClick={showMsg}>Click me</button>
+            <button onClick={()=>showMsg()}>Click me</button>
         </>
     )
 }
